89413f07849e223fa6e0a3b23b95668d4fdb35a1
TAEUNG-NAM <atrix_@naver.com>
2026-04-28 15:15:37 +0900

    feat: Circle Document 생성

---
+++ src/main/java/com/thinkfree/thinkchart/circle/domain/Circle.java
@@ -0,0 +1,35 @@
+package com.thinkfree.thinkchart.circle.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+@Builder
+@Document(collection = "circles")
+public class Circle {
+
+    @Id
+    private String id;
+
+    private double x;
+    private double y;
+
+    private double diameter;
+
+    @Builder.Default
+    private double red = 0.111;
+    @Builder.Default
+    private double green = 0.222;
+    @Builder.Default
+    private double blue = 0.333;
+    @Builder.Default
+    private double opacity = 0.9;
+
+    @CreatedDate
+    private Long createdAt;
+}
