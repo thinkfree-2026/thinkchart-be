319e470a02abc05d955de9655f3afe600309f024
TAEUNG-NAM <atrix_@naver.com>
2026-05-06 14:37:51 +0900

    feat: 차트 생성 요청 객체 구현

--- src/main/java/com/thinkfree/thinkchart/chart/dto/CreateChartRequest.java
+++ src/main/java/com/thinkfree/thinkchart/chart/dto/CreateChartRequest.java
@@ -1,5 +1,24 @@
 package com.thinkfree.thinkchart.chart.dto;
 
-public class CreateChartRequest {
+import com.thinkfree.thinkchart.chart.domain.Chart;
+import com.thinkfree.thinkchart.circle.domain.Circle;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "차트 생성 요청")
+public class CreateChartRequest {
+    private List<String> circleIds;
+
+    public Chart toEntity(List<Circle> circles) {
+        return Chart.builder()
+                .circles(circles)
+                .build();
+
+    }
 }
