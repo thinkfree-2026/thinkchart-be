56b0a73a2ee5bd2bee5ac31e1b54e96152fd00ec
TAEUNG-NAM <atrix_@naver.com>
2026-05-07 09:56:42 +0900

    refactor: Chart 생성 응답용 DTO(Response) 파일명 변경 및 구조 리팩토링

--- src/main/java/com/thinkfree/thinkchart/chart/dto/CreateChartResponse.java
+++ src/main/java/com/thinkfree/thinkchart/chart/dto/CreateChartResponse.java
@@ -1,29 +1,23 @@
 package com.thinkfree.thinkchart.chart.dto;
 
 import com.thinkfree.thinkchart.chart.domain.Chart;
-import com.thinkfree.thinkchart.circle.domain.Circle;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
-public class ChartResponse {
+public class CreateChartResponse {
     private String id;
     private String name;
-    private String xAxisName;
-    private String yAxisName;
     private List<String> circleIds;
     private Long createdAt;
 
-    public static ChartResponse from(Chart chart) {
-        return new ChartResponse(
+    public static CreateChartResponse from(Chart chart) {
+        return new CreateChartResponse(
                 chart.getId(),
                 chart.getName(),
-                chart.getXAxisName(),
-                chart.getYAxisName(),
                 chart.getCircleIds(),
                 chart.getCreatedAt()
         );
