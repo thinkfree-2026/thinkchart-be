dae973dc9c3dc3fbad0054c4671b1bb3b9f9e098
TAEUNG NAM <106743597+TAEUNG-NAM@users.noreply.github.com>
2026-05-08 18:12:06 +0900

    refactor: 스웨거API 문서 최신화 및 정렬 옵션 설정 (#22)

--- src/main/java/com/thinkfree/thinkchart/common/controller/WebSocketDocsController.java
+++ src/main/java/com/thinkfree/thinkchart/common/controller/WebSocketDocsController.java
@@ -6,36 +6,42 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 
-@Tag(name = "WebSocket API (문서용)", description = "실제로는 WebSocket(STOMP)으로 동작함. <br> 연결: ws://localhost:8080/ws-stomp")
+@Tag(name = "1. WebSocket API(문서용)", description = "실제로는 WebSocket(STOMP)으로 동작함. <br> 연결: ws://thinkchart.kro.kr:8080/ws")
 @RestController
 public class WebSocketDocsController {
 
 
-    @Operation(summary = "웹소켓 연결", description = "Destination: ws://localhost:8080/ws-stomp")
-    @RequestMapping(path = "/docs/websocket/connect", method = RequestMethod.OPTIONS)
+    @Operation(summary = "웹소켓 연결", description = "Destination: ws://thinkchart.kro.kr:8080/ws")
+    @RequestMapping(path = "ws://thinkchart.kro.kr:8080/ws", method = RequestMethod.OPTIONS)
     public void webSocketConnectDocs() {
         // 실제 로직은 구현하지 않음 (문서 노출용)
     }
 
-    @Operation(summary = "실시간 커서 공유 (WebSocket)", description = "Destination: /app/canvas/cursor <br> Subscribe: /topic/canvas")
+    @Operation(summary = "실시간 커서 공유", description = "Destination: /app/canvas/cursor <br> Subscribe: /topic/canvas")
     @RequestMapping(path = "/docs/app/canvas/cursor", method = RequestMethod.OPTIONS)
     public void cursorMoveDocs() {
         // 실제 로직은 구현하지 않음 (문서 노출용)
     }
 
-    @Operation(summary = "원 그리기 (WebSocket)", description = "Destination: /app/canvas/draw <br> Subscribe: /topic/canvas")
+    @Operation(summary = "차트 모달 커서 공유", description = "Destination: /app/canvas/{chartId}/cursor <br> Subscribe: /topic/canvas/{chartId}")
+    @RequestMapping(path = "/docs/app/canvas/{chartId}/cursor", method = RequestMethod.OPTIONS)
+    public void chartModalCursorMoveDocs() {
+        // 실제 로직은 구현하지 않음 (문서 노출용)
+    }
+
+    @Operation(summary = "원 그리기", description = "Destination: /app/canvas/draw <br> Subscribe: /topic/canvas")
     @RequestMapping(path = "/docs/app/canvas/draw", method = RequestMethod.OPTIONS)
     public void circleDrawDocs(@RequestBody UpdateCircleRequest request) {
         // 실제 로직은 구현하지 않음 (문서 노출용)
     }
 
-    @Operation(summary = "원 크기 조정 (WebSocket)", description = "Destination: /app/canvas/resize <br> Subscribe: /topic/canvas")
+    @Operation(summary = "원 크기 조정", description = "Destination: /app/canvas/resize <br> Subscribe: /topic/canvas")
     @RequestMapping(path = "/docs/app/canvas/resize", method = RequestMethod.OPTIONS)
     public void circleResizeDocs(@RequestBody UpdateCircleRequest request) {
         // 실제 로직은 구현하지 않음 (문서 노출용)
     }
 
-    @Operation(summary = "원 포커스 (WebSocket)", description = "Destination: /app/canvas/focus <br> Subscribe: /topic/canvas")
+    @Operation(summary = "원 포커스", description = "Destination: /app/canvas/focus <br> Subscribe: /topic/canvas")
     @RequestMapping(path = "/docs/app/canvas/focus", method = RequestMethod.OPTIONS)
     public void circleFocusDocs(@RequestBody UpdateCircleRequest request) {
         // 실제 로직은 구현하지 않음 (문서 노출용)
