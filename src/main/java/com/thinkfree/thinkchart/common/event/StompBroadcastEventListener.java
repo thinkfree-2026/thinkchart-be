06b5127d8e42b3c59eed7cea2bd54facd2848933
TAEUNG-NAM <atrix_@naver.com>
2026-05-11 11:06:48 +0900

    feat: 이벤트 수신 및 STOMP 메시지를 전송하는 이벤트 리스너 구현

--- src/main/java/com/thinkfree/thinkchart/common/event/StompBroadcastEventListener.java
+++ src/main/java/com/thinkfree/thinkchart/common/event/StompBroadcastEventListener.java
@@ -1,2 +1,22 @@
-package com.thinkfree.thinkchart.common.event;public class StompBroadcastEventListener {
+package com.thinkfree.thinkchart.common.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class StompBroadcastEventListener {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(StompBroadcastEvent event) {
+        messagingTemplate.convertAndSend(
+                event.getDestination(),
+                event.getMessage()
+        );
+    }
 }
