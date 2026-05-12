9b5468ed318eca7d62f98f076c7f54c41d329886
TAEUNG-NAM <atrix_@naver.com>
2026-05-12 09:26:22 +0900

    feat: 차트 바(Chart Bar) 수정 요청 DTO 생성

--- src/main/java/com/thinkfree/thinkchart/chart/dto/UpdateBarRequest.java
+++ src/main/java/com/thinkfree/thinkchart/chart/dto/UpdateBarRequest.java
@@ -1,5 +1,26 @@
 package com.thinkfree.thinkchart.chart.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "차트 바(원) 수정 요청")
 public class UpdateBarRequest {
+
+    @Schema(description = "값", example = "359.5")
+    @Positive
+    private Double value;
+
+    @Schema(description = "색상", example = "#000000")
+    @Pattern(
+            regexp = "^#[0-9a-fA-F]{6}$",
+            message = "올바른 색상 코드 형식이 아닙니다. (예: #000000)"
+    )
+    private String color;
 
 }
