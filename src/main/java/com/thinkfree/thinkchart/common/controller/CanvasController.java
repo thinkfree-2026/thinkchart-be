5a7388036cc4e8f2220f03493c773ab8554fb94e
TAEUNG-NAM <atrix_@naver.com>
2026-05-13 15:52:36 +0900

    feat: 사용자별 color 값 사용자 정보와 같이 브로드캐스팅

--- src/main/java/com/thinkfree/thinkchart/common/controller/CanvasController.java
+++ src/main/java/com/thinkfree/thinkchart/common/controller/CanvasController.java
@@ -1,27 +1,41 @@
 package com.thinkfree.thinkchart.common.controller;
 
+import com.thinkfree.thinkchart.common.dto.UserResponse;
 import com.thinkfree.thinkchart.common.event.WsAction;
 import com.thinkfree.thinkchart.common.event.WsMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class CanvasController {
 
     @MessageMapping("/canvas/join")
     @SendToUser(value = "/queue/me", broadcast = false)
-    public WsMessage<Map<String, String>> join(SimpMessageHeaderAccessor headerAccessor) {
-        String sessionId = headerAccessor.getSessionId();
-        Map<String, String> response = new HashMap<>();
-        response.put("userId", sessionId);
+    public WsMessage<UserResponse> join(SimpMessageHeaderAccessor headerAccessor) {
+
+        UserResponse response = new UserResponse(
+                headerAccessor.getSessionId(),
+                randomVisibleHexColor()
+        );
 
         return new WsMessage<>(WsAction.USER_ASSIGNED, response);
+    }
+
+    private String randomVisibleHexColor() {
+        int r = ThreadLocalRandom.current().nextInt(50, 231);
+        int g = ThreadLocalRandom.current().nextInt(50, 231);
+        int b = ThreadLocalRandom.current().nextInt(50, 231);
+
+        return String.format("#%02X%02X%02X", r, g, b);
     }
 }
