13737fddb42a6c0be70d189431c2122196c861ab
TAEUNG NAM <106743597+TAEUNG-NAM@users.noreply.github.com>
2026-05-19 13:28:32 +0900

    feat: 사용자 별 컬러 배정 방식 변경 및 커서 세션 종료 감지 (#49)
    * feat: 커서 세션 종료 이벤트 리스너 추가
    
    * feat: 커서 세션 종료 이벤트 리스너 추가
    
    * feat: STOMP 세션 연결 감지 및 ID 저장 로직 구현
    
    * refactor: 사용자별 컬러 배정 방식을 3색 순환 방식으로 변경 (랜덤Hex코드 -> 접속자 수 기준 순환)
    
    * refactor: 미사용 임포트 제거
    
    * refactor: 세션 종료 이벤트 리스너의 세션ID 획득 방식 통일화

--- src/main/java/com/thinkfree/thinkchart/common/controller/CanvasController.java
+++ src/main/java/com/thinkfree/thinkchart/common/controller/CanvasController.java
@@ -1,6 +1,7 @@
 package com.thinkfree.thinkchart.common.controller;
 
 import com.thinkfree.thinkchart.common.dto.UserResponse;
+import com.thinkfree.thinkchart.common.event.StompBroadcastEventListener;
 import com.thinkfree.thinkchart.common.event.WsAction;
 import com.thinkfree.thinkchart.common.event.WsMessage;
 import lombok.RequiredArgsConstructor;
@@ -9,29 +10,24 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
-import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 @RequiredArgsConstructor
 public class CanvasController {
 
+    private final StompBroadcastEventListener eventListener;
+
     @MessageMapping("/canvas/join")
     @SendToUser(value = "/queue/me", broadcast = false)
     public WsMessage<UserResponse> join(SimpMessageHeaderAccessor headerAccessor) {
 
+        int colorNumber = (eventListener.getConnectionCount() - 1)  % 3 + 1;
+
         UserResponse response = new UserResponse(
                 headerAccessor.getSessionId(),
-                randomVisibleHexColor()
+                String.valueOf(colorNumber)
         );
 
         return new WsMessage<>(WsAction.USER_ASSIGNED, response);
-    }
-
-    private String randomVisibleHexColor() {
-        int r = ThreadLocalRandom.current().nextInt(50, 231);
-        int g = ThreadLocalRandom.current().nextInt(50, 231);
-        int b = ThreadLocalRandom.current().nextInt(50, 231);
-
-        return String.format("#%02X%02X%02X", r, g, b);
     }
 }
--- src/main/java/com/thinkfree/thinkchart/common/event/StompBroadcastEventListener.java
+++ src/main/java/com/thinkfree/thinkchart/common/event/StompBroadcastEventListener.java
@@ -1,16 +1,27 @@
 package com.thinkfree.thinkchart.common.event;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @RequiredArgsConstructor
 public class StompBroadcastEventListener {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final Set<String> sessionIds = ConcurrentHashMap.newKeySet();
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(StompBroadcastEvent event) {
@@ -18,5 +29,31 @@ public class StompBroadcastEventListener {
                 event.getDestination(),
                 event.getMessage()
         );
+    }
+
+    @EventListener
+    public void handleConnect(SessionConnectedEvent event) {
+        String sessionId = SimpMessageHeaderAccessor.getSessionId(
+                event.getMessage().getHeaders()
+        );
+
+        if (sessionId != null) {
+            sessionIds.add(sessionId);
+        }
+    }
+
+    @EventListener
+    public void handleDisconnect(SessionDisconnectEvent event) {
+        String sessionId = event.getSessionId();
+        sessionIds.remove(sessionId);
+
+        messagingTemplate.convertAndSend(
+                "/topic/canvas",
+                new WsMessage<>(WsAction.CURSOR_LEAVE, Map.of("id", sessionId))
+        );
+    }
+
+    public int getConnectionCount() {
+        return sessionIds.size();
     }
 }
