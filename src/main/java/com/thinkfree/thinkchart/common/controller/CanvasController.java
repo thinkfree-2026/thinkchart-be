68ca145c80dbb470f3b0fa8ec58ac40d84ca12f8
TAEUNG-NAM <atrix_@naver.com>
2026-05-19 10:31:26 +0900

    refactor: 사용자별 컬러 배정 방식을 3색 순환 방식으로 변경 (랜덤Hex코드 -> 접속자 수 기준 순환)

--- src/main/java/com/thinkfree/thinkchart/common/controller/CanvasController.java
+++ src/main/java/com/thinkfree/thinkchart/common/controller/CanvasController.java
@@ -1,12 +1,14 @@
 package com.thinkfree.thinkchart.common.controller;
 
 import com.thinkfree.thinkchart.common.dto.UserResponse;
+import com.thinkfree.thinkchart.common.event.StompBroadcastEventListener;
 import com.thinkfree.thinkchart.common.event.WsAction;
 import com.thinkfree.thinkchart.common.event.WsMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Controller;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -15,23 +17,19 @@ import java.util.concurrent.ThreadLocalRandom;
 @RequiredArgsConstructor
 public class CanvasController {
 
+    private final StompBroadcastEventListener eventListener;
+
     @MessageMapping("/canvas/join")
     @SendToUser(value = "/queue/me", broadcast = false)
     public WsMessage<UserResponse> join(SimpMessageHeaderAccessor headerAccessor) {
 
+        int colorNumber = (eventListener.getConnectionCount() - 1)  % 3 + 1;
+
         UserResponse response = new UserResponse(
                 headerAccessor.getSessionId(),
-                randomVisibleHexColor()
+                String.valueOf(colorNumber)
         );
 
         return new WsMessage<>(WsAction.USER_ASSIGNED, response);
-    }
-
-    private String randomVisibleHexColor() {
-        int r = ThreadLocalRandom.current().nextInt(50, 231);
-        int g = ThreadLocalRandom.current().nextInt(50, 231);
-        int b = ThreadLocalRandom.current().nextInt(50, 231);
-
-        return String.format("#%02X%02X%02X", r, g, b);
     }
 }
