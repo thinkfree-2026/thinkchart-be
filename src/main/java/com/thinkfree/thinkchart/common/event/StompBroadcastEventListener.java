ce5b48aff5d291338215433f5097fe7785143c11
TAEUNG-NAM <atrix_@naver.com>
2026-05-19 10:26:07 +0900

    feat: STOMP 세션 연결 감지 및 ID 저장 로직 구현

--- src/main/java/com/thinkfree/thinkchart/common/event/StompBroadcastEventListener.java
+++ src/main/java/com/thinkfree/thinkchart/common/event/StompBroadcastEventListener.java
@@ -2,21 +2,26 @@ package com.thinkfree.thinkchart.common.event;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @RequiredArgsConstructor
 public class StompBroadcastEventListener {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final Set<String> sessionIds = ConcurrentHashMap.newKeySet();
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(StompBroadcastEvent event) {
@@ -27,12 +32,28 @@ public class StompBroadcastEventListener {
     }
 
     @EventListener
+    public void handleConnect(SessionConnectedEvent event) {
+        String sessionId = SimpMessageHeaderAccessor.getSessionId(
+                event.getMessage().getHeaders()
+        );
+
+        if (sessionId != null) {
+            sessionIds.add(sessionId);
+        }
+    }
+
+    @EventListener
     public void handleDisconnect(SessionDisconnectEvent event) {
+        sessionIds.remove(event.getSessionId());
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
 
         messagingTemplate.convertAndSend(
                 "/topic/canvas",
                 new WsMessage<>(WsAction.CURSOR_LEAVE, Map.of("id", Objects.requireNonNull(accessor.getSessionId())))
         );
+    }
+
+    public int getConnectionCount() {
+        return sessionIds.size();
     }
 }
